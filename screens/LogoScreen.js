c596b47cb8592cc41122a31ea1290749500bf0e0
SamsonI95 <samsoniweibo@gmail.com>
2024-01-24 16:23:54 +0100

    Starting files

---
+++ screens/LogoScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Logo = () => {
+  return (
+    <View>
+      <Text>Logo</Text>
+    </View>
+  )
+}
+
+export default Logo
